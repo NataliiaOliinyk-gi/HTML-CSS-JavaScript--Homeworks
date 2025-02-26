661338592be4dc13fb5026c8318e41dba5946782
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-02-26 19:45:24 +0100

    homework_06_1

---
+++ homework_06_1/main.js
@@ -0,0 +1,21 @@
+// Задания 1. 
+// Объявите две переменные firstName и lastName и присвойте им ваши имя и фамилию.
+// С помощью конкатенации строк создайте третью переменную greeting, содержащую приветствие в формате "Привет, Имя Фамилия!".
+// Выведите переменную greeting в консоль.
+
+let firstName = "Nataliia";
+let lastName = "Oliinyk";
+let greeting = "Привет, " + firstName + " " + lastName + "!"
+console.log(greeting);
+
+
+// Задания 2.*
+// Создайте переменную sentencePart1 и присвойте ей начало предложения (например, "Я люблю").
+// Создайте переменную sentencePart2 и присвойте ей завершение предложения (например, "программирование").
+// С помощью конкатенации строк создайте переменную completeSentence, объединяющую обе части предложения.
+// Выведите переменную completeSentence в консоль.
+
+let sentencePart1 = "Я люблю";
+let sentencePart2 = "программирование";
+let completeSentence = sentencePart1 + " " + sentencePart2;
+console.log(completeSentence);
